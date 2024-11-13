e0da751ae47fed4cc95f1f5fb6f43a9583bd214a
paxasonik <paxasonk@mail.ru>
2024-11-13 13:30:31 +0300

    ant-design installed and used

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,32 +1,45 @@
 <template>
-	<div>
-		<button @click="openModal">Открыть видео</button>
-		<div v-if="isModalOpened" class="modal">
-			<div>
-				<video
-					v-if="isPlayingVideo"
-					controls
-					:width="videoWidth"
-				>
-					<source src="/video-source.mp4" type="video/mp4" />
-					Ваш браузер не поддерживает видео.
-				</video>
-				<div class="controls">
-					<button @click="playingVideo">
-						{{ isPlayingVideo ? 'Пауза' : 'Воспроизвести' }}
-					</button>
-					<button @click="toggleModal">Изменить размер</button>
-					<button @click="closeModal">Закрыть</button>
-				</div>
-			</div>
-		</div>
+
+	<div class="closed-modal">
+		<PlayCircleOutlined class="playing-icon" @click="openModal"/>
 	</div>
-	{{snapshot}}
+
+	<Modal
+		v-model:open="isModalOpened"
+		:width="videoWidth"
+		title="PLAYER"
+		@ok="closeModal"
+	>
+		<template #footer>
+			<Button @click="toggleModal">
+				<template #icon>
+					<ArrowsAltOutlined v-if="isDefaultSize" />
+					<ShrinkOutlined v-else />
+				</template>
+			</Button>
+
+			<Button @click="handleVideo">
+				<template #icon>
+					<CaretRightFilled v-if="isPlayingVideo" />
+					<PauseOutlined v-else />
+				</template>
+			</Button>
+
+		</template>
+		<video
+			class="block-video"
+		>
+			<source src="/video-source.mp4" type="video/mp4" />
+			Ваш браузер не поддерживает видео.
+		</video>
+	</Modal>
 </template>
 
 <script setup>
 import { ref, computed } from 'vue';
 import { useMachine } from '@xstate/vue';
+import { Button, Modal } from 'ant-design-vue';
+import { CaretRightFilled, ShrinkOutlined, PauseOutlined, ArrowsAltOutlined, PlayCircleOutlined } from '@ant-design/icons-vue';
 import { videoPlayerMachine } from '@/machines/playerMachine.js';
 
 const { snapshot, send } = useMachine(videoPlayerMachine);
@@ -41,10 +54,12 @@ const modalOpenedSize = computed(() => {
 	return snapshot.value.value?.opened?.size;
 });
 
+const isDefaultSize = computed(() => {
+	return !!modalOpenedSize.value && modalOpenedSize.value !== 'default';
+});
+
 const videoWidth = computed(() => {
-	return !!modalOpenedSize.value && modalOpenedSize.value !== 'default'
-		? 500
-		: 1000;
+	return isDefaultSize.value ? 500 : 1000;
 });
 
 const openModal = () => {
@@ -58,19 +73,34 @@ const closeModal = () => {
 };
 
 const toggleModal = () => {
-	send(!!modalOpenedSize.value && modalOpenedSize.value !== 'default'
-		? { type: 'TOGGLE_DEFAULT' }
-		: { type: 'TOGGLE_SMALLER' });
+	send(isDefaultSize.value ? { type: 'TOGGLE_DEFAULT' } : { type: 'TOGGLE_SMALLER' });
 };
 
-const playingVideo = () => {
-	send(isPlayingVideo.value
-		? { type: 'TOGGLE_PAUSE' }
-		: { type: 'TOGGLE_PLAY' });
+const handleVideo = () => {
+	send(isPlayingVideo.value ? { type: 'TOGGLE_PAUSE' } : { type: 'TOGGLE_PLAY' });
 };
 
 </script>
 
 <style scoped>
+.closed-modal {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid #8585ff;
+    border-radius: 5px;
+    height: 150px;
+    width: 300px;
+    margin: 25vh auto auto
+}
 
+.playing-icon {
+    font-size: 50px;
+    color: #8585ff
+}
+
+.block-video {
+		width: 100%;
+		height: 100%;
+}
 </style>
